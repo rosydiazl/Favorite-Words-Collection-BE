0bbe8351f8a81b049abdac5467a30490aea82ff9
rosydiazl <rosyosmediazl@gmail.com>
2021-08-05 14:38:45 -0400

    Update previosuly named exampleSchema to wordsSchema and add word, definition, origin, language and sentence that will be required for user

---
+++ app/models/words.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose')
+
+const wordsSchema = new mongoose.Schema({
+  word: {
+    type: String,
+    required: true
+  },
+  definition: {
+    type: String,
+    required: true
+  },
+  origin: {
+    type: String,
+    required: true
+  },
+  language: {
+    type: String,
+    required: true
+  },
+  sentence: {
+    type: String,
+    required: true
+  },
+  owner: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true
+  }
+}, {
+  timestamps: true
+})
+
+module.exports = mongoose.model('Example', wordsSchema)
